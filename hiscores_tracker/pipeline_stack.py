b69b7f2b266c0decc3ab3b166bee8c77440ae8c3
awerchniak <andy.werchniak@gmail.com>
2026-06-12 20:51:24 -0400

    Fix synth: pin CDK CLI to 2.0.0 to match aws-cdk-lib and avoid Node 14 incompatibility

--- hiscores_tracker/pipeline_stack.py
+++ hiscores_tracker/pipeline_stack.py
@@ -56,8 +56,7 @@ class PipelineStack(Stack):
                 "Synth",
                 input=source,
                 commands=[
-                    "npm install -g n && n 18 && hash -r",
-                    "npm install -g aws-cdk@2",
+                    "npm install -g aws-cdk@2.0.0",
                     "pip install -r requirements.txt",
                     "cdk synth",
                 ],
